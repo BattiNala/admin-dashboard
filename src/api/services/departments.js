d058e02b53df22f21ba3dcd4ff7ced6f8032a9de
ankitdiyal17 <ankitdiyal17@gmail.com>
2026-05-09 15:59:32 +0545

    feat: update employee fetch endpoint

--- src/api/services/departments.js
+++ src/api/services/departments.js
@@ -20,7 +20,7 @@ export const listDepartmentAdmins = (departmentId = null) => {
 };
 
 export const listEmployees = () => {
-  return apiClient.get("/department/all-employees");
+  return apiClient.get("/superadmin/all-employees");
 };
 
 export const createDepartmentAdmin = (payload) => {
